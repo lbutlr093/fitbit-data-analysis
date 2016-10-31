fe77f6e86b6e0bb702102e0c7340f8e2b2d61409
lbutlr093 <lbutlr093@gmail.com>
2016-10-31 15:12:53 -0500

    simple csv input/output, no parsing yet

--- process-raw-files.py
+++ process-raw-files.py
@@ -3,12 +3,36 @@ import os
 import fnmatch
 
 ## collect each .csv file from directory
-rawFilesDir = '/home/logan/Documents/code/fitbit-data-analysis/fitbit-data-raw'  # temp hard-coded
-files = fnmatch.filter(os.listdir(rawFilesDir), '*.csv')
+rawFilesDir = './fitbit-data-raw/'  # temp hard-coded
+processedFilesDir = './fitbit-data-processed/'  # temp hard-coded
+inputFiles = fnmatch.filter(os.listdir(rawFilesDir), '*.csv')
+#print inputFiles
+
+## Setup readers and writers 
+#rawInput = 
+#bodyOutput = 
+#foodsOutput = 
+#activitiesOutput = 
+#sleepOutput = 
+testOutput = []
+outputList = []
 
 ## open each .csv file and split
-for file in files:
-    print file
-
-
-#with open()
+for file in inputFiles: # loop through each of the raw files
+    with open(rawFilesDir+file, 'rb') as f:
+        reader = csv.reader(f)
+        outputList = list(reader)
+        #for row in reader:
+            #testOutput += row
+    with open(processedFilesDir+'processed_'+file, 'wb') as f2:
+        writer = csv.writer(f2)
+        writer.writerows(outputList)
+		
+		
+#inputFile = open(rawFilesDir+'2015_05.csv', 'rb')
+#reader = csv.reader(inputFile)
+#for row in reader:
+#	if row == "Activities":
+#		print row
+#	print row
+#inputFile.close()
