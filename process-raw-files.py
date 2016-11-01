49354e1c7a0ef12bcd7b6557fc2fef39316ec919
lbutlr093 <lbutlr093@gmail.com>
2016-11-01 11:19:36 -0500

    splitting raw input files into body and activities files

--- process-raw-files.py
+++ process-raw-files.py
@@ -6,44 +6,22 @@ import fnmatch
 rawFilesDir = './fitbit-data-raw/'  # temp hard-coded
 processedFilesDir = './fitbit-data-processed/'  # temp hard-coded
 inputFiles = fnmatch.filter(os.listdir(rawFilesDir), '*.csv')
-#print inputFiles
-
-## Setup readers and writers 
-#rawInput = 
-#bodyOutput = 
-#foodsOutput = 
-#activitiesOutput = 
-#sleepOutput = 
-testOutput = []
-outputList = []
-test = 0
 
 ## open each .csv file and split
 for file in inputFiles: # loop through each of the raw files
     with open(rawFilesDir+file, 'rb') as f:
         reader = csv.reader(f)
         outputList = list(reader)
-    with open(processedFilesDir+'processed_'+file, 'wb') as f2:
-        writer = csv.writer(f2)
-        writer.writerows(outputList)
-		
-		
-#inputFile = open(rawFilesDir+'2015_05.csv', 'rb')
-#reader = csv.reader(inputFile)
-#for row in reader:
-#	if row == "Activities":
-#		print row
-#	print row
-#inputFile.close()
-
-
-
-
-## add in tomorrow
-#bodyList = []
-#Bodyobj = ['Body']
-#Activitiesobj = ['Activities']
-#BodyStarting = outputList.index(Bodyobj)
-#BodyEnding = outputList.index(Activities)
-#bodyList = outputList[BodyStarting+1:BodyEnding-1]
+    ## set up the indexes for each section in the raw .csv file
+    bodyList = outputList[outputList.index(['Body'])+1:outputList.index(['Activities'])-1]
+	# TODO: if sleep / food exist, conditionally set the following lists.
+	# TODO: foodsList = 
+    activitiesList = outputList[outputList.index(['Activities'])+1:len(outputList)-1]
+    # TODO: sleepList = 
+    with open(processedFilesDir+'body_'+file, 'wb') as bodyWriter:
+        writer = csv.writer(bodyWriter)
+        writer.writerows(bodyList) # changed from outputList
+    with open(processedFilesDir+'activities_'+file, 'wb') as activitiesWriter:
+        writer2 = csv.writer(activitiesWriter)
+        writer2.writerows(activitiesList) # change this to ActivitiesList
 
