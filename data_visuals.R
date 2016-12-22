1bdbd961e62d433c927a16f489c594be243ca068
lbutlr093 <lbutlr093@gmail.com>
2016-12-22 15:08:18 -0600

    changes to visuals - improved graphs

--- data_visuals.R
+++ data_visuals.R
@@ -5,6 +5,7 @@ data_visuals <- function() {
   ## different factors on my workout and general activity.
   
   library(ggplot2)
+  library(lubridate)
   
   ## TODO: hard-coded for now, will play with passing parameters later
   setwd("/home/logan/Documents/code/fitbit-data-analysis/fitbit_data_processed")
@@ -12,10 +13,36 @@ data_visuals <- function() {
   activities_data_frame <- read.csv("all_activities.csv")
   measurements_data_frame <- read.csv("all_measurements.csv")
   
-  ## Starting simple with histograms
-  distance_hist <- ggplot(activities_data_frame[which(activities_data_frame$Distance>0),], aes(x = Distance)) + geom_histogram()
-  ggsave(filename="../data_visualization/distance_histogram.png", plot=distance_hist)
+  activities_data_frame$Steps <- as.integer(sub(",", "", activities_data_frame$Steps))
+  activities_data_frame$Date <- as.Date(activities_data_frame$Date)
   
-  #need to convert steps column to numeric
-  #ggplot(activities_data_frame[which(activities_data_frame$Steps>0),], aes(x = Steps)) + geom_histogram()
+  ## Mean / Median without zero values
+  distance_median <- median(activities_data_frame$Distance[activities_data_frame$Distance > 0])
+  distance_mean <- mean(activities_data_frame$Distance[activities_data_frame$Distance > 0])
+  
+  ## Simple histogram with line indicating where the median is
+  distance_hist <- ggplot(activities_data_frame[which(activities_data_frame$Distance>0),], aes(x = Distance)) +
+    geom_histogram() +
+    ggtitle("Daily Distance with Median") +
+    labs(x = "Distance (miles)", y = "Days") +
+    theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=12)) +
+    theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=14, hjust=0.5)) +
+    geom_vline(xintercept = distance_median, color="blue")
+  ggsave(filename = "../data_visualization/distance_histogram.png", plot = distance_hist)
+  
+  ## Boxplot of distance (in miles) per month
+  distance_montly_boxplot <- ggplot(aes(y = Distance, x = factor(months(Date), levels = month.name)), data = activities_data_frame[which(activities_data_frame$Distance>0),]) + 
+    geom_boxplot() +
+    ggtitle("Monthly Distance") +
+    labs(x = "Month", y = "Distance (miles)") +
+    theme(axis.text.x = element_text(face="bold", color="#993333", size=12, angle=60, hjust=1),
+          axis.text.y = element_text(face="bold", color="#993333", size=12)) +
+    theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=14)) +
+    theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=16, hjust=0.5))
+  ggsave(filename = "../data_visualization/distance_by_month_boxplot.png", plot = distance_montly_boxplot)
+  
+  ## Future Steps graphs
+  #steps_hist <- ggplot(activities_data_frame[which(activities_data_frame$Steps>0),], aes(x - Steps)) + geom_histogram()
+  #ggsave(filename="../data_visualization/steps_histogram.png", plot=steps_hist)
+  
 }
